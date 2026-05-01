476aff84f853452d4dbe98b35bcc375d3d33adf2
argecyplusplus <tema.chernyuk@gmail.com>
2026-05-01 11:46:44 +0300

    исправлен материал (кроме начальных условий)

--- app/controllers/simulation_controller.py
+++ app/controllers/simulation_controller.py
@@ -5,8 +5,11 @@ from ..services.simulation_service import SimulationService
 from ..dto.simulation_dto import SimulationCreateRequest, InitialConditionCreateRequest
 from ..utils.database import get_db_session
 from ..models.blade import Blade, BladeAssembly
-from ..models.material import Material
 from ..models.simulation import InitialCondition
+from ..models.material import Material, AlloyComposition
+from ..repositories.material_repository import MaterialRepository
+from ..models.material import ChemicalElement  # добавить импорт
+from sqlalchemy.orm import joinedload
 from sqlalchemy import select
 
 sim_bp = Blueprint('simulation', __name__, url_prefix='/simulation')
@@ -20,13 +23,34 @@ def get_service():
 # ================= МОДЕЛИРОВАНИЕ =================
 @sim_bp.route('/')
 def index():
-    blades = g.db_session.scalars(select(Blade)).all()
-    assemblies = g.db_session.scalars(select(BladeAssembly)).all()
-    materials = g.db_session.scalars(select(Material)).all()
-    ics = get_service().get_initial_conditions_list()
-    sims = get_service().get_simulations_list()
-    return render_template('simulation.html', blades=blades, assemblies=assemblies,
-                           materials=materials, ics=ics, sims=sims)
+    service = get_service()
+    session = g.db_session
+
+    blades = session.scalars(select(Blade)).all()
+    assemblies = session.scalars(select(BladeAssembly)).all()
+
+    # Загружаем химические элементы (как на странице материалов)
+    chemical_elements = session.scalars(
+        select(ChemicalElement).options(joinedload(ChemicalElement.material))
+    ).all()
+    # Извлекаем связанные материалы
+    elements = [ce.material for ce in chemical_elements if ce.material]
+
+    # Сплавы – это материалы с is_alloy=True
+    alloys = session.scalars(
+        select(Material).where(Material.is_alloy == True)
+    ).all()
+
+    ics = session.scalars(select(InitialCondition)).all()
+    sims = service.get_simulations_list()
+
+    return render_template('simulation.html',
+                           blades=blades,
+                           assemblies=assemblies,
+                           elements=elements,
+                           alloys=alloys,
+                           ics=ics,
+                           sims=sims)
 
 @sim_bp.route('/create', methods=['POST'])
 def create():
